333c4c8a51b371b85d8d9c0c105b545d16a7f0c0
Alza Arhane <alza.arhane97@gmail.com>
2020-02-06 09:37:58 +0330

    added create review

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,44 +6,102 @@ const Task = require('../models/task');
 const Review = require('../models/review');
 
 router.get('/reviews/my_reviews', async (req, res) => {
-    const tasks = await Task
+    if (req.session.role === 'employer') {
+        const tasks = await Task
         .where('status')
         .equals(2)
         .where('employer_id')
         .equals(req.session._id)
         .sort({ created_at: 'desc' })
+        .skip(req.query.page ? (parseInt(req.query.page) - 1) * 4 : 0)
         .limit(4)
         .populate('freelancer_id')
         .exec();
-
-    const reviews = await Review    
+        
+        const reviews = await Review    
         .where('reviewer')
         .equals(req.session._id)
         .where('task')
         .in(tasks.map(task => task._id));
-    
-    let tempTasks = [];
-    for (let i = 0; i < tasks.length; i++) {
-        let sw = false;
-        for (let j = 0; j < reviews.length; j++) {
-            if ((tasks[i]._id).toString() === (reviews[j].task).toString()) {
-                sw = true;
+
+        let tempTasks = [];
+        for (let i = 0; i < tasks.length; i++) {
+            let sw = false;
+            for (let j = 0; j < reviews.length; j++) {
+                if ((tasks[i]._id).toString() === (reviews[j].task).toString()) {
+                    sw = true;
+                    tempTasks.push({
+                        task: tasks[i],
+                        review: reviews[j],
+                        review_created_at: reviews[j].created_at.toDateString(),
+                    });
+                    break;
+                }
+            }
+            if (!sw) {
                 tempTasks.push({
                     task: tasks[i],
-                    review: reviews[j],
-                    review_created_at: reviews[j].created_at.toDateString(),
                 });
-                break;
             }
-        }
-        if (!sw) {
-            tempTasks.push({
-                task: tasks[i],
-            });
-        }
+        }   
+        res.render('dashboard-reviews', { data: { tasks: tempTasks, currPage: req.query.page || 1 }, layout: false }); 
+    } else if (req.session.role === 'freelancer') {
+        const tasks = await Task
+        .where('status')
+        .equals(2)
+        .where('freelancer_id')
+        .equals(req.session._id)
+        .sort({ created_at: 'desc' })
+        .skip(req.query.page ? (parseInt(req.query.page) - 1) * 4 : 0)
+        .limit(4)
+        .populate('employer_id')
+        .exec();
+
+        const reviews = await Review    
+        .where('reviewee')
+        .equals(req.session._id)
+        .where('task')
+        .in(tasks.map(task => task._id));
+
+        let tempTasks = [];
+        for (let i = 0; i < tasks.length; i++) {
+            let sw = false;
+            for (let j = 0; j < reviews.length; j++) {
+                if ((tasks[i]._id).toString() === (reviews[j].task).toString()) {
+                    sw = true;
+                    tempTasks.push({
+                        task: tasks[i],
+                        review: reviews[j],
+                        review_created_at: reviews[j].created_at.toDateString(),
+                    });
+                    break;
+                }
+            }
+            if (!sw) {
+                tempTasks.push({
+                    task: tasks[i],
+                });
+            }
+            res.render('dashboard-reviews', { data: { tasks: tempTasks }, layout: false });
+        }  
+    } else {
+        return res.redirect('/');
     }
-    
-    res.render('dashboard-reviews', { data: { tasks: tempTasks }, layout: false });
+});
+
+router.post('/reviews', async (req, res) => {
+    if (req.session.role === 'employer') {
+        const rev = new Review({
+            reviewer: req.session._id,
+            reviewee: req.body.reviewee_id,
+            task: req.body.task_id,
+            score: req.body.rating,
+            comment: req.body.comment,
+            created_at: new Date(), 
+        });
+        await rev.save();
+        res.redirect('/reviews/my_reviews');
+    }
 });
 
 module.exports = router;
